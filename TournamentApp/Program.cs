7586ce41af4aa60ede104e924ad6da1f36f2042f
didrikm <didrik.molinder@protonmail.com>
2025-01-28 17:33:41 +0100

    it works. deployed to azure app service

--- TournamentApp/Program.cs
+++ TournamentApp/Program.cs
@@ -1,15 +1,38 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using TournamentApi.Extensions;
 using TournamentData.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddDbContext<TournamentApiContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString") ?? throw new InvalidOperationException("Connection string 'TournamentApiContext' not found.")));
+
 var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_DATABASE_CONNECTION_STRING")
     ?? throw new InvalidOperationException("Connection string environment variable 'ASPNETCORE_DATABASE_CONNECTION_STRING' not found.");
 
+builder.Services.AddDbContext<TournamentApiContext>(options =>
+    options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
+        maxRetryCount: 5,
+        maxRetryDelay: TimeSpan.FromSeconds(30),
+        errorNumbersToAdd: null)));
+
+//builder.Services.AddDbContext<TournamentApiContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"), sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
+//        maxRetryCount: 5,
+//        maxRetryDelay: TimeSpan.FromSeconds(30),
+//        errorNumbersToAdd: null)));
+
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 
 builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
     .AddNewtonsoftJson()
@@ -21,6 +44,8 @@ builder.Services.ConfigureServiceLayerServices();
 builder.Services.ConfigureRepositories();
 var app = builder.Build();
 
+app.UseCors("AllowAll");
+
 // Configure the HTTP request pipeline.
 //app.UseSwagger();
 //app.UseSwaggerUI();
@@ -31,5 +56,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "API is running");
 
 app.Run();
